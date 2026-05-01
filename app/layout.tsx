5832a2a9cf13ee419d6a0f974249815a3ca0c230
fakhri <fakhri@fakhris-MacBook-Pro.local>
2026-05-01 19:36:14 +0800

    fix: Layout web name

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'GNSS Early Warning Dashboard',
+  title: 'iLands — Intelligent Landslide Monitoring System',
   description: 'AI-Assisted Real-Time GNSS Monitoring & Landslide Early Warning System',
 }
 
